2ad753674caee9cd87b6b7bd178f9a56a5430dfd
wangzhehan84 <wangzhehan84@gmail.com>
2023-02-02 20:00:24 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "duanren's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/public' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -46,7 +46,7 @@ module.exports = {
                     '/public/书籍'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: '',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
